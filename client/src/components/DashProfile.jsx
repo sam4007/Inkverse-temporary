7667b6564572025d5d0eb447fa8caccb4fc62b84
sam4007 <learningsam4007@gmail.com>
2024-07-07 14:44:35 +0530

    Complete create a post page UI (Add react-quill package and update index.css)

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -8,10 +8,11 @@ import 'react-circular-progressbar/dist/styles.css';
 import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 
 export default function DashProfile() {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -133,7 +134,7 @@ export default function DashProfile() {
       });
       const data = await res.json();
       if (!res.ok) {
-       console.log(data.message);
+        console.log(data.message);
       } else {
         dispatch(signoutSuccess());
       }
@@ -164,7 +165,14 @@ export default function DashProfile() {
         <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} onChange={handleChange} />
         <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange} />
         <TextInput type='password' id='password' placeholder='password' onChange={handleChange} />
-        <Button type='submit' gradientDuoTone='purpleToBlue' className='self-center' outline>Update</Button>
+        <Button type='submit' gradientDuoTone='purpleToBlue' className='w-full' outline disabled={loading || imageFileUploading}>{loading ? 'Loading...' : 'Update'}</Button>
+        {
+          currentUser.isAdmin && (
+            <Link to={'/create-post'}>
+              <Button type='button' gradientDuoTone='purpleToPink' className='w-full'>Create a post</Button>
+            </Link>
+          )
+        }
       </form>
       <div className='flex justify-center gap-4 mt-5 text-red-600'>
         <span onClick={() => setShowModal(true)} className='cursor-pointer hover:underline underline-offset-4'>Delete Account</span> |
