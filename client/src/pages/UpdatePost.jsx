ed103dd3fafbb03afff44f14eb4e5909f843de97
sam4007 <learningsam4007@gmail.com>
2024-08-02 19:18:24 +0530

    Update : Updated update post page UI and functionality

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -104,25 +104,38 @@ export default function UpdatePost() {
     };
 
     return (
-        <div className='p-3 max-w-3xl mx-auto min-h-screen'>
-            <h1 className='text-center text-3xl my-7 font-semibold'>Update post</h1>
+        <div className='p-3 max-w-7xl mx-auto min-h-screen'>
+            <h1 className='text-center text-5xl my-7 font-bold'>Update post</h1>
             <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
                 <div className='flex flex-col gap-4 sm:flex-row justify-between'>
                     <TextInput type='text' placeholder='Title' required id='title' className='flex-1' onChange={(e) => setFormData({ ...formData, title: e.target.value })} value={formData.title} />
                     <Select onChange={(e) => setFormData({ ...formData, category: e.target.value })} value={formData.category}>
                         <option value={'uncategorized'}>Select a category</option>
-                        <option value={'javascript'}>JavaScript</option>
-                        <option value={'javascript'}>React</option>
-                        <option value={'javascript'}>Node</option>
+                        <option value={'science-technology'}>Science & Technology</option>
+                        <option value={'travel-lifestyle'}>Travel & Lifestyle</option>
+                        <option value={'health-wellness'}>Health & Wellness</option>
+                        <option value={'food-recipes'}>Food & Recipes</option>
+                        <option value={'business-finance'}>Business & Finance</option>
+                        <option value={'entertainment-culture'}>Entertainment & Culture</option>
+                        <option value={'education-learning'}>Education & Learning</option>
+                        <option value={'opinion-current-affairs'}>Opinion & Current Affairs</option>
+                        <option value={'creative-arts'}>Creative Arts</option>
+                        <option value={'reviews-recommendations'}>Reviews & Recommendations</option>
+                        <option value={'others'}>Others...</option>
                     </Select>
                 </div>
-                <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
+                <div className='flex gap-4 items-center justify-between border-2 border-blue-500 p-3 rounded-xl'>
                     <FileInput type='file' accept='image/*' onChange={(e) => setFile(e.target.files[0])} />
                     <Button type='button' gradientDuoTone='purpleToBlue' size='sm' outline onClick={handleUploadImage} disabled={imageUploadProgress}>
                         {
                             imageUploadProgress ? (
                                 <div className='w-16 h-16'>
-                                    <CircularProgressbar value={imageUploadProgress} text={`${imageUploadProgress || 0}%`} />
+                                    <CircularProgressbar value={imageUploadProgress} text={`${imageUploadProgress || 0}%`} strokeWidth={5}
+                                        styles={{
+                                            root: { width: '100%', height: '100%', position: 'absolute', top: 0, left: 0 },
+                                            path: { stroke: `rgba(56,176,0,${imageUploadProgress / 100})` },
+                                            text: { fill: 'rgba(56,176,0)', fontSize: '20px' }
+                                        }} />
                                 </div>
                             ) : ('Upload Image')
                         }
@@ -133,11 +146,11 @@ export default function UpdatePost() {
                     <img
                         src={formData.image}
                         alt='upload'
-                        className='w-full h-72 object-cover'
+                        className='w-full h-80 object-cover'
                     />
                 )
                 }
-                <ReactQuill theme='snow' value={formData.content} placeholder='write something...' className='h-72 mb-12' required onChange={(value) => setFormData({ ...formData, content: value })} />
+                <ReactQuill theme='snow' value={formData.content} placeholder='write something...' className='h-72 mb-12 text-black dark:text-white' required onChange={(value) => setFormData({ ...formData, content: value })} />
                 <Button type='submit' gradientDuoTone='purpleToPink'>Update</Button>
                 {publishError && <Alert className='mt-5' color='failure'>{publishError}</Alert>}
             </form>
