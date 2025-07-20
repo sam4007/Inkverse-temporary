0898ea373060bf94c0d2ad0675e7f113774b7e4b
sam4007 <learningsam4007@gmail.com>
2025-07-20 18:52:21 +0530

    Simplify Vite config and use npm install --force to fix module resolution

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,25 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:3000',
-        secure: false,
-        changeOrigin: true,
-      },
-    },
-  },
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
   },
   plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom'],
-  },
 })
